3d7a988785daf3a6e7345b12674df0cce0a1712c
Praneeth Bonagiri <praneeethbonagiri.pb@gmail.com>
2024-05-25 23:53:03 +0530

    Added spring and configuring using xml file

--- src/main/java/com/learning/springcore/Mobile.java
+++ src/main/java/com/learning/springcore/Mobile.java
@@ -1,9 +1,37 @@
 package com.learning.springcore;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Mobile {
     public static void main(String[] args) {
 
-        Sim sim = new Jio();
+        /*
+        This is how we normally write java code
+        Where ever we need some class we create object of that class
+        and use it.
+
+        The problem with this approach is if I need to change the sim,
+        I need to change the source code
+
+        This indicates our application is not configurable
+         */
+//        Sim sim = new Airtel();
+//
+//        sim.call();
+//        sim.internet();
+
+        /*
+        Now let us use spring framework in our application
+        Spring makes our application more configurable, and it manages all the
+        beans/objects needed for our application.
+
+        Instead of us creating the objects we can instruct spring to create
+        and provide us.
+         */
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("beans.xml");
+
+        Sim sim = appContext.getBean("sim", com.learning.springcore.Sim.class);
 
         sim.call();
         sim.internet();
